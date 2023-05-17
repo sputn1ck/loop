d482345edb4b84913ca4210ad9267e0e74b28c6e
sputn1ck <kon@kon.ninja>
2023-05-17 19:03:51 +0200

    loopdb: generate sqlc files

--- loopdb/sqlc/models.go
+++ loopdb/sqlc/models.go
@@ -4,9 +4,71 @@
 
 package sqlc
 
-import ()
+import (
+	"time"
+)
+
+type HtlcKey struct {
+	SwapHash               []byte
+	SenderScriptPubkey     []byte
+	ReceiverScriptPubkey   []byte
+	SenderInternalPubkey   []byte
+	ReceiverInternalPubkey []byte
+	ClientKeyFamily        int32
+	ClientKeyIndex         int32
+}
+
+type LiquidityParam struct {
+	ID     int32
+	Params []byte
+}
+
+type LoopinSwap struct {
+	SwapHash       []byte
+	HtlcConfTarget int32
+	LastHop        []byte
+	ExternalHtlc   bool
+}
+
+type LoopoutSwap struct {
+	SwapHash            []byte
+	DestAddress         string
+	SwapInvoice         string
+	MaxSwapRoutingFee   int64
+	SweepConfTarget     int32
+	HtlcConfirmations   int32
+	OutgoingChanSet     string
+	PrepayInvoice       string
+	MaxPrepayRoutingFee int64
+	PublicationDeadline time.Time
+}
 
 type Macaroon struct {
 	ID      []byte
 	RootKey []byte
+}
+
+type Swap struct {
+	ID               int32
+	SwapHash         []byte
+	Preimage         []byte
+	InitiationTime   time.Time
+	AmountRequested  int64
+	CltvExpiry       int32
+	MaxMinerFee      int64
+	MaxSwapFee       int64
+	InitiationHeight int32
+	ProtocolVersion  int32
+	Label            string
+}
+
+type SwapUpdate struct {
+	ID              int32
+	SwapHash        []byte
+	UpdateTimestamp time.Time
+	UpdateState     int32
+	HtlcTxhash      string
+	ServerCost      int64
+	OnchainCost     int64
+	OffchainCost    int64
 }
--- loopdb/sqlc/querier.go
+++ loopdb/sqlc/querier.go
@@ -9,8 +9,20 @@ import (
 )
 
 type Querier interface {
+	FetchLiquidityParams(ctx context.Context) ([]byte, error)
+	GetLoopInSwap(ctx context.Context, swapHash []byte) (GetLoopInSwapRow, error)
+	GetLoopInSwaps(ctx context.Context) ([]GetLoopInSwapsRow, error)
+	GetLoopOutSwap(ctx context.Context, swapHash []byte) (GetLoopOutSwapRow, error)
+	GetLoopOutSwaps(ctx context.Context) ([]GetLoopOutSwapsRow, error)
 	GetRootKey(ctx context.Context, id []byte) (Macaroon, error)
+	GetSwapUpdates(ctx context.Context, swapHash []byte) ([]SwapUpdate, error)
+	InsertHtlcKeys(ctx context.Context, arg InsertHtlcKeysParams) error
+	InsertLoopIn(ctx context.Context, arg InsertLoopInParams) error
+	InsertLoopOut(ctx context.Context, arg InsertLoopOutParams) error
 	InsertRootKey(ctx context.Context, arg InsertRootKeyParams) error
+	InsertSwap(ctx context.Context, arg InsertSwapParams) error
+	InsertSwapUpdate(ctx context.Context, arg InsertSwapUpdateParams) error
+	UpsertLiquidityParams(ctx context.Context, params []byte) error
 }
 
 var _ Querier = (*Queries)(nil)
